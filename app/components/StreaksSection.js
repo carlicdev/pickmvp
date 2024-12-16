44742bcd8de3b23c415618649e1ea57032c01220
carlos camacho <carlic.dev@gmail.com>
2024-12-16 07:00:36 -0600

    added pick

--- app/components/StreaksSection.js
+++ app/components/StreaksSection.js
@@ -4,27 +4,31 @@ import Streak from './Streak'
 
 const StreaksSection = () => {
   return (
-    <div className='max-w-7xl mx-auto w-full flex flex-wrap -mt-48 mb-20 justify-center'>
-      <div className='w-full lg:w-1/3 my-5 lg:my-0'>
-      <Streak 
-            title={'all sports'}
-            data={'3-2'}
-            timelapse={'ayer'}
-          />
-      </div>
-      <div className='w-full lg:w-1/3 my-5 lg:my-0'>
+    <div className='max-w-7xl mx-auto h-auto bg-cover bg-center overflow-hidden my-10 rounded-lg' style={{ backgroundImage: 'url(./warriors-nba.jpg)'}}>
+      <div className='bg-slate-900 bg-opacity-70 h-full lg:p-20 p-5'>
+        <div className='max-w-7xl mx-auto w-full flex flex-wrap justify-center'>
+          <div className='w-full lg:w-1/3 my-5 lg:my-0'>
           <Streak 
-            title={'NFL'}
-            data={'2-1'}
-            timelapse={'ayer'}
-          />
+                title={'all sports'}
+                data={'3-2'}
+                timelapse={'ayer'}
+              />
+          </div>
+          <div className='w-full lg:w-1/3 my-5 lg:my-0'>
+              <Streak 
+                title={'NFL'}
+                data={'2-1'}
+                timelapse={'ayer'}
+              />
+          </div>
+          <div className='w-full lg:w-1/3 my-5 lg:my-0'>
+          <Streak 
+                title={'NBA'}
+                data={'1-1'}
+                timelapse={'ayer'}
+              />
+          </div>
       </div>
-      <div className='w-full lg:w-1/3 my-5 lg:my-0'>
-      <Streak 
-            title={'NBA'}
-            data={'1-1'}
-            timelapse={'ayer'}
-          />
       </div>
   </div>
   )
